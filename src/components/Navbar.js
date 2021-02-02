12e1661b62bf953584d641a37052f77297dbd535
Lyzer <lyzersmurf@gmail.com>
2021-02-02 23:22:51 +0800

    fixed navbar sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
 import brand from './images/roy_plaza_store_online_logo.svg'
+import smallBrand from './images/small_logo.png'
 import search from './images/search_button.svg'
 import cart from './images/shopping_cart_icon.svg'
 import bell from './images/bell_icon.svg'
 import user from './images/user_profile_icon.svg'
 import styles from './navbar.module.css'
+import './hamburger.css'
 
 class Navbar extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             searchBody : ''
+             searchBody : '',
+             toggle: ''
         }
         this.searchInputTop = React.createRef()
         this.searchInputBottom = React.createRef()
+
         this.cancelHandlerTop = this.cancelHandlerTop.bind(this)
         this.cancelHandlerBottom = this.cancelHandlerBottom.bind(this)
+        this.hamburgerToggle = this.hamburgerToggle.bind(this)
     }
     
     cancelHandlerTop(){
@@ -26,6 +31,15 @@ class Navbar extends Component {
     cancelHandlerBottom(){
         this.searchInputBottom.current.value = ''
     }
+
+    hamburgerToggle(e){
+        let value = this.state.toggle === ''? 'open' : ''
+
+        this.setState({
+            toggle: value
+        })
+    }
+
 
     searchOnChangeHandler = (e) => {
         this.setState({
@@ -37,8 +51,22 @@ class Navbar extends Component {
     render() {
         return (
             <div className={styles.Navbar}>
+
+
                 <div className={styles.navbarTop}>
-                    <a href="#"><img src={brand} className={styles.brand}/></a>
+                    <div className={styles.hamBrandContainer}>
+                        <div id="nav-icon3" className={this.state.toggle} onClick={this.hamburgerToggle}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className={styles.brandContainer}>
+                            <a href="#"><img src={brand} className={styles.brand}/></a>
+                            <a href="#"><img src={smallBrand} className={styles.smallBrand}/></a>
+                        </div>
+                    </div>
+
 
                     <form className={styles.formTop}>
                         <input type="text" placeholder="Search in ROY Plaza Store Online" ref={this.searchInputTop} className={styles.input} value={this.state.searchBody} onChange={this.searchOnChangeHandler}/>
